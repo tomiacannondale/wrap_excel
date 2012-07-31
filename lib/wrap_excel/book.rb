cbe04bf25dc9f3d04c9fa2f52788dd0d2cc3701f
tomi <tomiacannondale@gmail.com>
2012-07-31 21:03:58 +0900

    Change local variable(options) to instance variable
    WrapExcel::Book#save use options[:read_only].

--- lib/wrap_excel/book.rb
+++ lib/wrap_excel/book.rb
@@ -5,7 +5,7 @@ module WrapExcel
     attr_reader :book
 
     def initialize(file, options={ }, &block)
-      options = {
+      @options = {
         :read_only => true,
         :displayalerts => false,
         :visible => false
@@ -13,10 +13,10 @@ module WrapExcel
       file = WrapExcel::Cygwin.cygpath('-w', file) if RUBY_PLATFORM =~ /cygwin/
       file = WIN32OLE.new('Scripting.FileSystemObject').GetAbsolutePathName(file)
       @winapp = WIN32OLE.new('Excel.Application')
-      @winapp.DisplayAlerts = options[:displayalerts]
-      @winapp.Visible = options[:visible]
+      @winapp.DisplayAlerts = @options[:displayalerts]
+      @winapp.Visible = @options[:visible]
       WIN32OLE.const_load(@winapp, WrapExcel) unless WrapExcel.const_defined?(:CONSTANTS)
-      @book = @winapp.Workbooks.Open(file,{ 'ReadOnly' => options[:read_only] })
+      @book = @winapp.Workbooks.Open(file,{ 'ReadOnly' => @options[:read_only] })
 
       if block
         begin
