cf19485c26e48f1c6501723c0daa356a13d5a8af
tomi <tomiacannondale@gmail.com>
2013-03-10 18:03:28 +0900

    Add caputure for test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,17 @@ end
 def rm_tmp(tmpdir)
   FileUtils.remove_entry_secure(File.dirname(tmpdir))
 end
+
+# This method is almost copy of wycats's implementation.
+# http://pochi.hatenablog.jp/entries/2010/03/24
+def capture(stream)
+  begin
+    stream = stream.to_s
+    eval "$#{stream} = StringIO.new"
+    yield
+    result = eval("$#{stream}").string
+  ensure
+    eval("$#{stream} = #{stream.upcase}")
+  end
+  result
+end
